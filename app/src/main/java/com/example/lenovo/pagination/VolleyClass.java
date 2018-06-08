bb1061ea675dfab4d9d483d011520841d8ce5773
pooja467 <pooja.sangwan134@gmail.com>
2018-06-08 14:18:46 +0530

    Volley Example

--- app/src/main/java/com/example/lenovo/pagination/VolleyClass.java
+++ app/src/main/java/com/example/lenovo/pagination/VolleyClass.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.Request;
@@ -11,26 +12,33 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class VolleyClass extends AppCompatActivity {
 TextView textView;
+ImageView image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volley_class);
         textView=(TextView)findViewById(R.id.textview);
-        String url = "https://gulllak.com/gulllaktest/resources/gulllakapi/versioncheck.php";
+        image=(ImageView)findViewById(R.id.image);
+        String url = "https://gulllak.com/efs/resources/gulllakapi/authanticatelogin.php";
         RequestQueue queue= Volley.newRequestQueue(this);
 
-        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        StringRequest jsonObjectRequest=new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
             @Override
-            public void onResponse(JSONObject response) {
+            public void onResponse(String response) {
 
                 Log.d("edwaess",response.toString());
                 textView.setText("Response => "+response.toString());
+
                 //findViewById(R.id.progressBar1).setVisibility(View.GONE);
             }
         }, new Response.ErrorListener() {
@@ -39,7 +47,24 @@ TextView textView;
 
             }
 
-    });
+    })
+        {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+
+
+                params.put("username","pooja.sangwan134@gmail.com");
+                params.put("password","467@pooja");
+
+
+
+                return params;
+            }
+
+        }
+
+                ;
 
         queue.add(jsonObjectRequest);
     }
